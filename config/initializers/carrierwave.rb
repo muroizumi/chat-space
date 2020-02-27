e2ed890c3272d9fe3c0a787ecb59bbff5d262d1d
muroizumi <muroizumi@gmail.com>
2020-02-27 12:04:03 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chatspace0831'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace0831'
 end
